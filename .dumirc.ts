44cfe0029a5953156a15149d5e4321b76a07ed8c
liulidong <lidong.liu@hand-china.com>
2023-02-20 14:37:20 +0800

    init

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'dumi';
 
 export default defineConfig({
-  base: '/learning-doc/',
+  base: '/learning-doc',
   publicPath: '/learning-doc/',
   outputPath: 'docs-dist',
   themeConfig: {
